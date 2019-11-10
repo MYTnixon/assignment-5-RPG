2f5dcddf647853fb9c2703846d7d9794eba43cf2
nickcampoli <dacuel21@gmail.com>
2019-11-10 01:58:50 -0500

    Added top-down movement

---
+++ Assignment-5-RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    Vector3 vector;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector = Vector3.zero;
+        vector.x = Input.GetAxisRaw("Horizontal");
+        vector.y = Input.GetAxisRaw("Vertical");
+        if (vector != Vector3.zero)
+        {
+            Move();
+            if (Input.GetKey("a"))
+            {
+                animator.Play("Player_Run");
+                spriteRenderer.flipX = true;
+            }
+            else if (Input.GetKey("d"))
+            {
+                animator.Play("Player_Run");
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            animator.Play("Player_Idle");
+        }
+    }
+
+    void Move()
+    {
+        rbody.MovePosition(transform.position + vector * speed * Time.deltaTime);
+    }
+}
